dbb07d8296fb041f0a27f0d4b2b684df593f6042
y2w8 <yousefalharby416@gmail.com>
2026-03-27 20:17:25 +0300

    add: logger

---
+++ src/logger.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use log::LevelFilter;
+use simplelog::{CombinedLogger, Config, WriteLogger};
+use std::fs::File;
+use std::path::PathBuf;
+use std::{env, fs};
+
+pub fn init() -> Result<()> {
+    let level = match env::var("RUST_LOG")
+        .unwrap_or_default()
+        .to_lowercase()
+        .as_str()
+    {
+        "trace" => LevelFilter::Trace,
+        "debug" => LevelFilter::Debug,
+        "info" => LevelFilter::Info,
+        "warn" => LevelFilter::Warn,
+        "error" => LevelFilter::Error,
+        _ => return Ok(()), // logging disabled
+    };
+    let log_file = File::create(get_log_path()).expect("Failed to create log file.");
+
+    CombinedLogger::init(vec![WriteLogger::new(level, Config::default(), log_file)]).unwrap();
+    Ok(())
+}
+
+pub fn get_log_path() -> PathBuf {
+    let base = dirs::cache_dir().unwrap_or_else(|| PathBuf::from(".")); // fallback to cwd
+
+    let dir = base.join("netrs");
+    fs::create_dir_all(&dir).expect("Failed to create log dir");
+    dir.join("netrs.log")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
+use std::io::stdout;
+
+#[macro_use] extern crate log;
+extern crate simplelog;
 
 mod app;
+mod logger;
 mod tui;
 mod ui;
 mod nm;
@@ -13,6 +18,8 @@ async fn main() -> Result<()> {
     // Initialize app state
     let mut app = App::new().await?;
     let mut tui = Tui::new()?;
+    // Initialize
+    logger::init()?;
 
     // Start TUI
     tui.run(&mut app).await?;
